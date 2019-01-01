d509138d6f1da4b24be2e77e462fba436fa71b9f
James Claar <jamesdclaar@gmail.com>
2019-01-01 17:38:15 -0600

    Update config.php

--- config.php
+++ config.php
@@ -2,13 +2,7 @@
 
 //api settings
 
-// Dad
-$api_token = "c5ed5a7190f6a7d68d66d1869f7691b2";
-
-// James
-// $api_token = "9ba62cb1cdf43827ace25af60748d88a";
-$url = "https://www.toggl.com/api/v8/time_entries/current";
-
+$api_token = "Your API token here";
 
 //-------------------------------------------------------------------
 //
